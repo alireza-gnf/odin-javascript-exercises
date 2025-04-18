c65735e7f45e2bf7a15911baac8c6520c864dc14
Alireza Ghazanfari <alirezagnf99@gmail.com>
2025-04-18 16:31:47 +0330

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (numA, numB) {
+  if (
+    numA < 0 ||
+    numB < 0 ||
+    parseInt(numA) !== numA ||
+    parseInt(numB) !== numB
+  ) {
+    return "ERROR";
+  }
+  [numA, numB] = [...[numA, numB].sort()];
+  let sum = 0;
+  for (let i = numA; i <= numB; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
