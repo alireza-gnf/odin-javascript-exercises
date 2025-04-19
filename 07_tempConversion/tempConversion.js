1285872458cad24148201f9aa8ae7e5b14a946b9
Alireza Ghazanfari <alirezagnf99@gmail.com>
2025-04-19 11:00:49 +0330

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (t) {
+  return parseFloat(((t - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (t) {
+  return parseFloat((t * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
