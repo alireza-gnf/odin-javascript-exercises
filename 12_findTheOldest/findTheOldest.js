4ef0f7030e6771ed0c9ffc28e06f1e20cea65188
Alireza Ghazanfari <alirezagnf99@gmail.com>
2025-04-19 14:51:54 +0330

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // sort in descending order
+  const sortedByAge = [...people].sort(
+    (personA, personB) =>
+      calculatePersonAge(personB) - calculatePersonAge(personA)
+  );
 
+  return sortedByAge[0];
+};
+
+const CURRENT_YEAR = new Date().getFullYear();
+const calculatePersonAge = (person) => {
+  // if there are people without year of death, set current year to calculate age.
+  if (!person.yearOfDeath) {
+    return CURRENT_YEAR - person.yearOfBirth;
+  }
+  return person.yearOfDeath - person.yearOfBirth;
 };
 
 // Do not edit below this line
