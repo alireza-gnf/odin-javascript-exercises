3e249e69fdd5333882717c62cf179ec6cdd98d7a
Alireza Ghazanfari <alirezagnf99@gmail.com>
2025-04-19 12:53:38 +0330

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const onlyAlphaNumeric = string.replace(/[\W_]/g, "").toLowerCase();
+  return onlyAlphaNumeric === onlyAlphaNumeric.split("").reverse().join("");
 };
 
 // Do not edit below this line
