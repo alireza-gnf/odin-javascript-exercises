8bdf0cadf9f1f9bfa79cfe262149af1c9cbb4d77
Alireza Ghazanfari <alirezagnf99@gmail.com>
2025-04-19 13:06:56 +0330

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const sequence = [0, 1, 1];
 
+const fibonacci = function (nth) {
+  if (nth < 0) {
+    return "OOPS";
+  }
+  if (sequence[nth] === undefined) {
+    sequence[nth] = fibonacci(nth - 1) + fibonacci(nth - 2);
+  }
+  return sequence[nth];
 };
 
 // Do not edit below this line
